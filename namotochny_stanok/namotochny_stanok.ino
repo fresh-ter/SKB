a32140f23c66533854df6b4bdccca9116480d538
Terminator (SKB) <alexey.orlov2016@yandex.ru>
2019-08-01 19:50:54 +0400

    Добавил управление по COM-port

--- namotochny_stanok/namotochny_stanok.ino
+++ namotochny_stanok/namotochny_stanok.ino
@@ -22,8 +22,18 @@
 #include <Adafruit_PCD8544.h>
 Adafruit_PCD8544 display = Adafruit_PCD8544(3, 4, 5, 6, 7);
 
+int mode = 0;
+enum statuses {CONF, WORK, STOP};
+char *menu_str[] =
+{
+  "CONF",
+  "WORK",
+  "STOP"
+};
+
+
 int counter = 0;
-int counter_static = 10;
+int counter_static = 0;
 int time_start = 0; // в секундах
 int time_end = 0; // в секундах
 String value;
@@ -56,6 +66,14 @@ void setup() {
   digitalWrite(STEP_PIN, 1);
   digitalWrite(DIR_PIN, 0);
 
+  pinMode(MULTIPLEXER_S0, OUTPUT);
+  pinMode(MULTIPLEXER_S1, OUTPUT);
+  pinMode(MULTIPLEXER_S2, OUTPUT);
+
+  pinMode(MULTIPLEXER_D0, INPUT);
+  digitalWrite(MULTIPLEXER_D0, HIGH);
+  pinMode(MULTIPLEXER_D1, INPUT);
+  digitalWrite(MULTIPLEXER_D1, HIGH);
 
   //  // Крутимся в одну сторону
   //  digitalWrite(DIR_PIN, HIGH);
@@ -66,104 +84,90 @@ void setup() {
   //    digitalWrite(STEP_PIN, LOW);
   //    delay(DELAY_STEPS);
   //  }
+
+  play_display();
 }
 
 void loop() {
-  //  if (Serial.available() > 0) {  //если есть доступные данные
-  //    // считываем байт
-  //    value = Serial.readString();
+
+  if (Serial.available() > 0) {  //если есть доступные данные
+    // считываем байт
+    value = Serial.readString();
+
+    if (value == "set_turns")
+    {
+      while (true)
+      {
+        if (Serial.available() > 0) {  //если есть доступные данные
+          // считываем байт
+          String s = Serial.readString();
+          counter_static = s.toInt();
+          break;
+        }
+      }
+    }
+    else if (value == "start")
+    {
+      abc();
+    }
+    
+    play_display();
+  }
+
+
+  //  get_data_multiplexer();
   //
-  //    //value = atoi(incomingByte);
+  //  Serial.print(b1[0]);
+  //  Serial.print(" ");
+  //  Serial.print(b1[1]);
+  //  Serial.print(" ");
+  //  Serial.print(b1[2]);
+  //  Serial.print(" ");
+  //  Serial.print(b1[3]);
+  //  Serial.print(" ");
+  //  Serial.print(b1[4]);
+  //  Serial.print(" ");
+  //  Serial.print(b1[5]);
+  //  Serial.print(" ");
+  //  Serial.print(b1[6]);
+  //  Serial.print(" ");
+  //  Serial.println(b1[7]);
   //
+  //  Serial.print(b2[0]);
+  //  Serial.print(" ");
+  //  Serial.print(b2[1]);
+  //  Serial.print(" ");
+  //  Serial.print(b2[2]);
+  //  Serial.print(" ");
+  //  Serial.print(b2[3]);
+  //  Serial.print(" ");
+  //  Serial.print(b2[4]);
+  //  Serial.print(" ");
+  //  Serial.print(b2[5]);
+  //  Serial.print(" ");
+  //  Serial.print(b2[6]);
+  //  Serial.print(" ");
+  //  Serial.println(b2[7]);
   //
+  //  Serial.println();
   //
-  //    if (value == "100")
-  //    {
-  //      digitalWrite(DIR_PIN, LOW);
-  //      //  1 оборот
-  //      for (int i = 0; i < STEP_ROUND; i++) {
-  //        digitalWrite(STEP_PIN, HIGH);
-  //        delay(DELAY_STEPS);
-  //        digitalWrite(STEP_PIN, LOW);
-  //        delay(DELAY_STEPS);
-  //      }
-  //    }
-  //  }
-
-  
-  get_data_multiplexer();
-
-  Serial.print(b1[0]);
-  Serial.print(" ");
-  Serial.print(b1[1]);
-  Serial.print(" ");
-  Serial.print(b1[2]);
-  Serial.print(" ");
-  Serial.print(b1[3]);
-  Serial.print(" ");
-  Serial.print(b1[4]);
-  Serial.print(" ");
-  Serial.print(b1[5]);
-  Serial.print(" ");
-  Serial.print(b1[6]);
-  Serial.print(" ");
-  Serial.println(b1[7]);
-  
-  Serial.print(b2[0]);
-  Serial.print(" ");
-  Serial.print(b2[1]);
-  Serial.print(" ");
-  Serial.print(b2[2]);
-  Serial.print(" ");
-  Serial.print(b2[3]);
-  Serial.print(" ");
-  Serial.print(b2[4]);
-  Serial.print(" ");
-  Serial.print(b2[5]);
-  Serial.print(" ");
-  Serial.print(b2[6]);
-  Serial.print(" ");
-  Serial.println(b2[7]);
-
-  Serial.println();
-
-  delay(1000);
-  
-
-
-  //  // Крутимся в одну сторону
-  //  digitalWrite(DIR_PIN, HIGH);
-  //  //  1 оборот
-  //  for (int i = 0; i < STEP_ROUND; i++) {
-  //    digitalWrite(STEP_PIN, HIGH);
-  //    delay(DELAY_STEPS);
-  //    digitalWrite(STEP_PIN, LOW);
-  //    delay(DELAY_STEPS);
-  //  }
-  //  // Затем меняем направление и крутимся в другую сторону
-  //  digitalWrite(DIR_PIN, LOW);
-  //  // сделать 1 оборот
-  //  for (int i = 0; i < STEP_ROUND; i++) {
-  //    digitalWrite(STEP_PIN, HIGH);
-  //    delay(DELAY_STEPS);
-  //    digitalWrite(STEP_PIN, LOW);
-  //    delay(DELAY_STEPS);
-  //  }
+  //  delay(1000);
 }
 
 void abc()
 {
+  mode = 1;
   digitalWrite(DIR_PIN, LOW);
 
   for (counter; counter <= counter_static; counter++)
   {
-    if(counter == 0)
-      time_start = millis()/1000;
-    if(counter == 1)
-      time_end = (millis()/1000 - time_start)*counter_static;
+    if (counter == 0)
+      time_start = millis() / 1000;
+    if (counter == 1)
+      time_end = (millis() / 1000 - time_start) * counter_static;
 
     play_display();
-    
+
     //  1 оборот
     for (int i = 0; i < STEP_ROUND; i++) {
       digitalWrite(STEP_PIN, HIGH);
@@ -175,14 +179,14 @@ void abc()
 
 void get_data_multiplexer()
 {
-  for(int i=0; i < 8; i++)
+  for (int i = 0; i < 8; i++)
   {
-    digitalWrite(MULTIPLEXER_S0, GET_BIT(i, 0));
-    digitalWrite(MULTIPLEXER_S1, GET_BIT(i, 1));
-    digitalWrite(MULTIPLEXER_S2, GET_BIT(i, 2));
+    digitalWrite(MULTIPLEXER_S0, bitRead(i, 0));
+    digitalWrite(MULTIPLEXER_S1, bitRead(i, 1));
+    digitalWrite(MULTIPLEXER_S2, bitRead(i, 2));
 
-    b1[i] = analogRead(MULTIPLEXER_D0);
-    b2[i] = analogRead(MULTIPLEXER_D1);
+    b1[i] = digitalRead(MULTIPLEXER_D0);
+    b2[i] = digitalRead(MULTIPLEXER_D1);
   }
 }
 
@@ -197,8 +201,8 @@ void play_display()
   //display.setTextColor(BLACK); // установка цвета текста
   display.setCursor(0, 0); // установка позиции курсора
 
-  display.println("Mode: ###");
+  display.println("Mode: " + String(menu_str[mode]));
   display.println("Turns: " + String(counter) + "/" + String(counter_static));
-  display.println("Time: " + String((millis()/1000 - time_start)) + "/" + String(time_end));
+  display.println("Time: " + String((millis() / 1000 - time_start)) + "/" + String(time_end));
   display.display();
 }
